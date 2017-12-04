342cf52d2a85cc0f28b93efa0c6fc6cd84a1812f
sebastian arias <jsarias98@misena.edu.co>
2017-12-04 08:38:07 -0500

    frontend

--- data/frontend/pages/index.vue
+++ data/frontend/pages/index.vue
@@ -6,7 +6,7 @@
         mutualcoin
       </h1>
       <h2 class="subtitle">
-        aplicacion para inversion en criptomonedas
+        aplicación para inversión en criptomonedas
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
