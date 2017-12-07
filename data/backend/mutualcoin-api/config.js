765e5d4fe33cd1c02c16f30557d2252807abd2ff
Omar Plata <oommaarr@misena.edu.co>
2017-12-07 17:08:53 -0500

    Install dependecies command

--- data/backend/mutualcoin-api/config.js
+++ data/backend/mutualcoin-api/config.js
@@ -7,6 +7,6 @@ module.exports = {
     password: process.env.DBPASSWORD || 'k0qo9sh9r8bkm9fscvb5h7rfhs',
     db: process.env.DBNAME || 'heroku_wdxc31kk',
     port: process.env.DBPORT || 33136
-    },
-  secret: process.env.SECRET || 'mutualcoin' 
+  },
+  secret: process.env.SECRET || 'mutualcoin'
 }
