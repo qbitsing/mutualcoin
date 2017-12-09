6e198d6a3a977873d8355993420cac065c9c18de
sebastian arias <jsarias98@misena.edu.co>
2017-12-09 11:16:26 -0500

    store

--- data/frontend/plugins/axios.js
+++ data/frontend/plugins/axios.js
@@ -1,6 +1,17 @@
 import axios from 'axios'
 const api = function (resource, data, methods) {
-  const instance = axios.create({ baseURL: 'http://localhost:3300/api/' })
+  var instance = null
+  if (localStorage.getItem('token') != null) {
+    instance = axios.create({
+      baseURL: 'http://localhost:3300/api/',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+      }
+    })
+  } else {
+    instance = axios.create({ baseURL: 'http://localhost:3300/api/' })
+  }
+  console.log(instance)
   if (methods === 'get') {
     return instance.get(resource, data)
   }
