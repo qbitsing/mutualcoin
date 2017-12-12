cc47e3f0460a77d7d9e275cf99992d174e8aeb69
Omar Plata <oommaarr@misena.edu.co>
2017-12-12 15:34:57 -0500

    coin fixes

--- data/backend/mutualcoin-api/routes/coin.js
+++ data/backend/mutualcoin-api/routes/coin.js
@@ -9,70 +9,70 @@ const ensure = require('express-jwt')
 
 let coinModel
 
-api.use('*', (req, res, next) => { 
-    if (!coinModel) { 
-        coinModel = req.db.coin
-    }
+api.use('*', (req, res, next) => {
+  if (!coinModel) {
+    coinModel = req.db.coin
+  }
 
-    next()
+  next()
 })
 
 api.get('/all',
     ensure({ secret: config.secret }),
-    async (req, res, next) => { 
-        debug('a request has come to api/coins/all')
-        if (!req.user.admin) {  
-            return next(new Error('Unauthorized'))
-        }
-        let coins = []
-        try {
-            coins = await coinModel.get()
-        } catch (error) {
-            return next(error)
-        }
+    async (req, res, next) => {
+      debug('a request has come to api/coins/all')
+      if (!req.user.admin) {
+        return next(new Error('Unauthorized'))
+      }
+      let coins = []
+      try {
+        coins = await coinModel.get()
+      } catch (error) {
+        return next(error)
+      }
 
-        res.send({ coins })
-    }   
+      res.send({ coins })
+    }
 )
 api.post('/create',
     ensure({ secret: config.secret }),
-    async (req, res, next) => { 
-        debug('a request has come to api/coins/create')
-        if (!req.user.admin) {  
-            return next(new Error('Unauthorized'))
-        } 
-        const { coinToCreate } = req.body
-        if (!coinToCreate) { 
-            return next(new Error('bad request: please specify the coin to create'))
-        }
-        let coinCreated
-        try {
-            coinCreated = await coinModel.create(coinToCreate)
-        } catch (error) {
-            return next(error)
-        }
+    async (req, res, next) => {
+      debug('a request has come to api/coins/create')
+      if (!req.user.admin) {
+        return next(new Error('Unauthorized'))
+      }
+      const { coinToCreate } = req.body
+      if (!coinToCreate) {
+        return next(new Error('bad request: please specify the coin to create'))
+      }
+      let coinCreated
+      try {
+        coinCreated = await coinModel.create(coinToCreate)
+      } catch (error) {
+        return next(error)
+      }
 
-        res.send({ coinCreated })
+      res.send({ coinCreated })
     }
 )
 
 api.put('/update/:uuid',
     ensure({ secret: config.secret }),
-    async (req, res, next) => { 
-        const { uuid } = req.params
-        debug(`a request has come to api/coins/update/${uuid}`)
-        if (!req.user.admin) {  
-            return next(new Error('Unauthorized'))
-        }
-        const { coinToUpdate } = req.body
-        let coinUpdated
-        try {
-            coinUpdated = await coinModel.update(uuid, userToUpdate)
-        } catch (error) {
-            return next(error)
-        }
+    async (req, res, next) => {
+      const { uuid } = req.params
+      debug(`a request has come to api/coins/update/${uuid}`)
+      if (!req.user.admin) {
+        return next(new Error('Unauthorized'))
+      }
+      const { coinToUpdate } = req.body
+      let coinUpdated
+      try {
+        coinUpdated = await coinModel.update(uuid, coinToUpdate)
+      } catch (error) {
+        return next(error)
+      }
 
-        res.send({ coinUpdated })
+      res.send({ coinUpdated })
     }
 )
 
