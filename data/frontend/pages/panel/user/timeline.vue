d1d2031e69f2fb36de7483135582ca073959fafc
Nicolás Arias <nmarias38@misena.edu.co>
2017-12-13 18:25:30 -0500

    Timeline

--- data/frontend/pages/panel/user/timeline.vue
+++ data/frontend/pages/panel/user/timeline.vue
@@ -2,9 +2,25 @@
   <v-layout column>
     <v-card>
       <v-card-title >
-        Activo
+        {{state}}
       </v-card-title>
-        <mutual-timeline></mutual-timeline>
+        <mutual-timeline :data="data"></mutual-timeline>
+        <v-stepper v-model="e1">
+          <v-stepper-header>
+            <v-stepper-step
+            v-for="(week, index) in weeks"
+            :step="index+1"
+            :editable=""
+            :complete="(e1 >= (index+1))"
+
+          ></v-stepper-step>
+          </v-stepper-header>
+          <v-stepper-items>
+            <v-stepper-content v-for="(week, index) in weeks" :step="index+1">
+              <v-card color="grey lighten-1" class="mb-5" height="200px">{{e1}}</v-card>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
     </v-card>
   </v-layout>
 </template>
@@ -15,7 +31,30 @@
     layout: 'dashboard',
     data () {
       return {
-        dialog: false
+        e1: 3,
+        data: {
+          per: 80,
+          start: '25/03/2017',
+          finish: '17/06/2017'
+        },
+        state: 'Activo',
+        weeks: [{
+          alto: 4,
+          medio: 3,
+          bajo: 1
+        }, {
+          alto: 4,
+          medio: 3,
+          bajo: 1
+        }, {
+          alto: 4,
+          medio: 3,
+          bajo: 1
+        }, {
+          alto: 4,
+          medio: 3,
+          bajo: 1
+        }]
       }
     },
     components: { MutualTimeline },
