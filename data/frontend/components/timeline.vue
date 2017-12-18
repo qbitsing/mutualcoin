8e28ac50798cf8f75285ff35e250f0126bb2c07b
Nicolás Arias <nmarias38@misena.edu.co>
2017-12-18 10:07:02 -0500

    peticiones

--- data/frontend/components/timeline.vue
+++ data/frontend/components/timeline.vue
@@ -3,17 +3,21 @@
     <v-container grid-list-md text-xs-center>
       <v-layout row>
         <v-flex xs1 center>
-          <div class="date">
-            {{data.start}}
-          </div>
+          <v-layout align-center justify-center class="max-height">
+            <v-card dark class="date">
+              {{data.start}}
+            </v-card>
+          </v-layout>
         </v-flex>
         <v-flex xs10>
           <v-progress-linear :value="(data.dPass*100)/(data.nWeeks*7)" height="15" color="primary"></v-progress-linear>
         </v-flex>
         <v-flex xs1 center>
-          <div class="date">
-            {{data.start}}
-          </div>
+          <v-layout align-center justify-center class="max-height">
+            <v-card dark class="date">
+              {{data.start}}
+            </v-card>
+          </v-layout>
         </v-flex>
       </v-layout>
     </v-container>
@@ -36,11 +40,15 @@
   }
 </script>
 <style lang="css">
-  .start{
-    width: 35px;
-    height: 35px;
-    background: rgb(8, 163, 61);
-    border-radius: 50%;
-    cursor: default;
+.max-height{
+  height: 100%;
+}
+.date {
+  padding: 4px;
+}
+@media (max-width: 1260px) {
+  .date {
+    font-size: 12px !important;
   }
+}
 </style>
