fb02aa48327c2403d13f79224fe3f2927e221074
sebastian arias <jsarias98@misena.edu.co>
2017-12-20 11:16:04 -0500

    blocks inversion

--- data/frontend/pages/panel/admin/bloqueinversion.vue
+++ data/frontend/pages/panel/admin/bloqueinversion.vue
@@ -1,12 +1,34 @@
 <template>
   <section>
-    <v-container grid-list-md>
-      <v-layout row wrap>
-        <v-flex xs6 sm4 lg3 v-for="block in blocks" :key="block.reference" class="pt-2">
-          <mutual-block  :data="block"  />
-        </v-flex>
-      </v-layout>
-    </v-container>
+    <v-card class="mb-2">
+      <v-card-title class="mutual-title">
+        <h2>Bloques en inversion</h2>
+      </v-card-title>
+      <v-card-text class="no-padding-top-bottom">
+        <v-container grid-list-md>
+          <v-layout row wrap>
+            <v-flex xs6 sm4 lg3 v-for="block in blocks" :key="block.reference" class="pt-2">
+              <mutual-block  :data="block"  />
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+    </v-card>
+    <v-card>
+      <v-card-title class="mutual-title">
+        <h2>Bloques Invertidos</h2>
+      </v-card-title>
+      <v-card-text class="no-padding-top-bottom">
+        <v-container grid-list-md>
+          <v-layout row wrap>
+            <v-flex xs6 sm4 lg3 v-for="block in blocks" :key="block.reference" class="pt-2">
+              <mutual-block  :data="block"  />
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+    </v-card>
+    
   </section>
 </template>
 <script>
@@ -29,7 +51,7 @@ export default {
   components: {MutualBlock},
   methods: {
     async getBlock () {
-      const res = await api('block/active', {}, 'get', this.$store.state.authToken)
+      const res = await api('block/state', {}, 'get', this.$store.state.authToken, {params: {active: 'active', waiting: 'waiting'}})
       if (res.status === 200) {
         this.coins = this.$store.state.coins
         this.blocks = res.data.blocks
