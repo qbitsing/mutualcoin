522d93161af47c9746a4ddb11ee7950e779b4e5a
Omar Plata <oommaarr@misena.edu.co>
2018-01-15 16:19:35 -0500

    Cambios

---
+++ data/backend/mutualcoin-api/io/emit-message.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+const socket = require('./')
+
+module.exports = function (message) {
+  socket().then(client => { 
+    client.emit('message', message)
+  }).catch(error => { 
+    console.error(`Error en la conexion con el servidor en tiempo real: ${err.message}`)
+  })
+}
