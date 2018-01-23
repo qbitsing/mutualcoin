cec347fcae09b8b54cf67f479adaaaedc7a073f2
Nicolas Arias <nmarias38@misena.edu.co>
2018-01-23 15:58:06 -0500

    cambios

--- data/frontend/components/ticket.vue
+++ data/frontend/components/ticket.vue
@@ -1,51 +1,53 @@
 <template>
   <section>
-    <v-layout row>
-        <v-flex xs12>
-          <v-form ref="message" v-model="valid">
-              <v-card>
-                <v-card-title class="mutual-title">
-                  <h2>Asunto: My Awsome Issue</h2>
-                </v-card-title>
-                <v-card-text class="no-padding-bottom">
-                  <v-text-field 
-                  textarea
-                  :rules="[v => !!v || 'Debe escribir un mensaje.']"
-                  v-model="message"
-                  label="Escriba su respuesta">
-                  </v-text-field>
-                  <file-chooser :imageData="imageData"></file-chooser>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn color="primary" @click="ask">Responder</v-btn>
-                  <v-btn color="warning">Cerrar Ticket</v-btn>
-                </v-card-actions>
-              </v-card>
-          </v-form>
-        </v-flex>
-    </v-layout>
-    <v-layout row v-for="message in messages" :key="message.uuid" :justify-end="user ? message.admin : !message.admin" class="message">
-      <v-flex xs11 sm9 md7>
-        <v-card flat :color="message.admin ? 'blue lighten-2' : 'amber lighten-2'">
-          <v-layout row>
-            <v-card-title class="no-padding-bottom">
-              <h3>{{message.admin ? 'Admin' : 'User'}}</h3>
-            </v-card-title>
-          </v-layout>
-          <v-layout row wrap>
-            <v-flex xs12 v-if="message.imageBase64" @click="showImage(message.imageBase64)">
-              <img :src="message.imageBase64" alt="imagen" class="white">
-            </v-flex>
-            <v-flex xs12>
-              <v-card-text>
-                {{message.message}}
-              </v-card-text>
-            </v-flex>
-          </v-layout>
-          <span class="date">{{message.date}}</span>
-        </v-card>  
-      </v-flex>
-    </v-layout>
+    <v-form ref="message" v-model="valid">
+        <v-card>
+          <v-card-title class="mutual-title">
+            <h2>Asunto: My Awsome Issue</h2>
+          </v-card-title>
+          <v-container class="messages" ref="chat">
+            <v-layout row v-for="message in messages" :key="message.uuid" :justify-end="user ? message.admin : !message.admin" class="message">
+              <v-flex xs11 sm9 md7>
+                <v-card flat :color="message.admin ? 'blue lighten-2' : 'amber lighten-2'">
+                  <v-layout row>
+                    <v-card-title class="no-padding-bottom">
+                      <h3>{{message.admin ? 'Admin' : 'User'}}</h3>
+                    </v-card-title>
+                  </v-layout>
+                  <v-layout row>
+                    <v-flex xs3 v-if="message.imageBase64" @click="showImage(message.imageBase64)">
+                      <div class="image-container">
+                      <div class="message-image" :style="'background-image: url('+ message.imageBase64 + ')'"></div>
+                      </div>
+                    </v-flex>
+                    <v-flex :xs9="message.imageBase64 ? true : false" :xs12="message.imageBase64 ? false : true">
+                      <v-card-text>
+                        {{message.message}}
+                      </v-card-text>
+                    </v-flex>
+                  </v-layout>
+                  <span class="date">{{message.date}}</span>
+                </v-card>  
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <v-card-text>
+            <v-text-field 
+            textarea
+            rows="2"
+            class="message-text"
+            :rules="[v => !!v || 'Debe escribir un mensaje.']"
+            v-model="message"
+            label="Escriba su respuesta">
+            </v-text-field>
+            <file-chooser :imageData="imageData"></file-chooser>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" @click="ask">Responder</v-btn>
+            <v-btn color="warning">Cerrar Ticket</v-btn>
+          </v-card-actions>
+        </v-card>
+    </v-form>
     <v-dialog v-model="dialog" content-class="ticket-modal" scrollable>
       <img class="modal-image" :src="modalImage" alt="">
     </v-dialog>
@@ -100,10 +102,44 @@
         this.modalImage = image
         this.dialog = true
       }
+    },
+    mounted () {
+      this.$refs.chat.scrollTo(0, this.$refs.chat.scrollHeight)
     }
   }
 </script>
+<style>
+  .message-text {
+    padding-top: 0;
+  }
+  .message-text label {
+    top: 14px !important;
+  }
+  .message-text textarea{
+    font-size: 13px !important;
+  }
+</style>
+
 <style scoped>
+  .message-image {
+    height: 100%;
+    transition: .3s;
+    cursor: pointer;
+    background-size: cover;
+    background-position: center;
+  }
+  .image-container {
+    height: 100%;
+    padding: 16px;
+    padding-right: 0;
+  }
+  .message-image:hover {
+    opacity: .7;
+  }
+  .messages {
+    overflow-y: scroll;
+    height: 50vh;
+  }
   .modal-image {
     align-self: center;
   }
